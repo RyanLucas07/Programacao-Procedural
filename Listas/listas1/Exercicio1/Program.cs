05c1b459023050e1d5a0db285a2044930628cc35
RyanLucas07 <ryan.lucas1717@outlook.com>
2024-02-21 22:43:26 -0300

    atualizacao das pastas

---
+++ Listas/listas1/Exercicio1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Qual sua primeira nota?:");
+           double nota1 = double.Parse(Console.ReadLine());
+        Console.WriteLine("Qual sua segunda nota?:");
+           double nota2 = double.Parse(Console.ReadLine());
+        Console.WriteLine("Qual sua terceira nota?:");
+           double nota3 = double.Parse(Console.ReadLine());
+        Console.WriteLine("Qual sua quarta nota?:");
+           double nota4 = double.Parse(Console.ReadLine());
+
+        double resultado = (nota1 + nota2 + nota3 + nota4) / 4.0;
+
+        Console.WriteLine("A media do aluno eh: " + resultado);
+        
+    }
+}
---
+++ Listas/listas1/Exercicio2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program
+{
+   static void Main(string[] args)
+   {
+    Console.WriteLine("Qual a base do triangulo?:");
+      double base1 = double.Parse(Console.ReadLine());
+    Console.WriteLine("Qual a altura do triangulo?:");
+      double altura = double.Parse(Console.ReadLine());
+
+      double area = (base1 * altura) / 2;
+      bool resultado = area > 20;
+    
+    Console.WriteLine("A area do triangulo eh maior que 20?: " + resultado );
+
+    }
+
+}
+
