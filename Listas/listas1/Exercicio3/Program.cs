cadce66a073c8bd0214cdd13b3c2a1abd61aa064
RyanLucas07 <ryan.lucas1717@outlook.com>
2024-02-21 22:29:34 -0300

    lista 1 com 3 exercicios

---
+++ Listas/listas1/Exercicio3/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+       Console.WriteLine("Digite quantos centavos deseja converter a Reais!: ");
+        int dinheiro = int.Parse(Console.ReadLine());
+        
+        int reais = dinheiro / 100;
+        int centavos = dinheiro % 100;
+
+        Console.WriteLine("O total em dinheiro eh: " + reais + " real(is) e " + centavos + " centavo(s)");
+    }
+}
