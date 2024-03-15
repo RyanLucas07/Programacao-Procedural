b752e2168130861485d84b93b3c280a4366d794a
RyanLucas07 <ryan.lucas1717@outlook.com>
2024-03-15 12:29:24 -0300

    lista4

---
+++ Listas/lista4/exercicio2/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        const int MENOR_TAMANHO_ALTURA = 1;
+        const int MAIOR_TAMANHO_ALTURA = 9;
+
+        for (int contador = 1; contador == contador; contador++)
+        {
+            System.Console.WriteLine("Digite a altura deseja: ");
+            int tamanhoAltura = int.Parse(Console.ReadLine());
+
+            if(tamanhoAltura >= MENOR_TAMANHO_ALTURA && tamanhoAltura <= MAIOR_TAMANHO_ALTURA)
+            {
+                for(int linha = 1; linha <=tamanhoAltura; linha++)
+                {
+            
+                    for (int coluna = 1; coluna <= tamanhoAltura; coluna++)
+                    {            
+
+                        if(coluna <= linha)
+                        {
+                            Console.Write(coluna);
+                        }
+
+                        
+                                    
+                    }
+                    System.Console.WriteLine();
+                }
+            break;   
+            }
+            else 
+            {
+                System.Console.WriteLine("Alturta invalida, digite uma altura entre um e nove!");
+            }
+        }
+    }
+}
