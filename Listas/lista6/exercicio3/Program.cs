e624338ee3721d27a41e69578e99cf41e6be46c0
RyanLucas07 <ryan.lucas1717@outlook.com>
2024-03-30 20:37:47 -0300

    Lista6

---
+++ Listas/lista6/exercicio3/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        const int TAMANHO_MATRIZ = 3;
+        int somatorio = 0;
+
+        int [,] matriz = new int[TAMANHO_MATRIZ, TAMANHO_MATRIZ];
+
+        for(int linha = 0; linha < TAMANHO_MATRIZ; linha++)
+        {
+            for(int coluna = 0; coluna < TAMANHO_MATRIZ; coluna++)
+            {
+                System.Console.WriteLine("Digite um numero ");
+                matriz[linha, coluna] = int.Parse(Console.ReadLine());
+            }
+        }
+
+        for(int linha = 0; linha < TAMANHO_MATRIZ; linha++)
+        {
+            for(int coluna = 0; coluna < TAMANHO_MATRIZ; coluna++)
+            {
+                if(linha == coluna)
+                {
+                    somatorio = somatorio + matriz[linha, coluna];
+                }
+            }
+        }
+        System.Console.WriteLine(somatorio);
+    }
+}
---
+++ Listas/lista6/exercicio5/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        const int TAMANHO_VETOR = 10;
+        const int TAMANHO_MINIMO = 10;
+        const int TAMANHO_MAXIMO = 50;
+
+        int [] numerosDigitados = new int[TAMANHO_VETOR];
+
+        int contador = 0;
+        while(contador < TAMANHO_VETOR)
+        {
+            System.Console.WriteLine("Digite um numero: ");
+            int numero = int.Parse(Console.ReadLine());
+
+            if(numero >= TAMANHO_MINIMO && numero <= TAMANHO_MAXIMO)
+            {
+                numerosDigitados[contador] = numero;
+                contador++;
+            }
+
+        }
+        
+        for(contador = 0; contador < TAMANHO_VETOR; contador++)
+        {
+            System.Console.WriteLine(numerosDigitados[contador]);
+        }
+    }
+}
