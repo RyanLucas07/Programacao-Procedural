d29b346010e41a874c3f295a53f3daad779459f2
RyanLucas07 <ryan.lucas1717@outlook.com>
2024-04-11 20:28:18 -0300

    lista8

---
+++ Listas/lista8/exercicio1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+struct Pizza
+{
+    public int id;
+    public double valor;
+    public string sabor;
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+        Pizza pizzaDigitada;
+        System.Console.WriteLine("Digite o id da pizza: ");
+        pizzaDigitada.id = int.Parse(Console.ReadLine());
+
+        System.Console.WriteLine("Digite o sabor da pizza: ");
+        pizzaDigitada.sabor = Console.ReadLine();
+
+        System.Console.WriteLine("Digite o valor da pizza: ");
+        pizzaDigitada.valor = double.Parse(Console.ReadLine());
+
+        System.Console.WriteLine(pizzaDigitada.id);
+        System.Console.WriteLine(pizzaDigitada.sabor);
+        System.Console.WriteLine(pizzaDigitada.valor);
+    }
+}
---
+++ Listas/lista8/exercicio2/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+struct Produto
+{
+    public int id;
+    public double valor;
+    public string nome;
+    public bool disponivelEmEstoque;
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+        const int TAMANHO_VETOR = 4;
+
+        Produto[] produtos = new Produto[TAMANHO_VETOR];
+
+        for(int i = 0; i < produtos.Length; i++)
+        {
+            System.Console.WriteLine("Digite o id do produto");
+            produtos[i].id = int.Parse(Console.ReadLine());
+
+            System.Console.WriteLine("Digite o nome do produto");
+            produtos[i].nome = Console.ReadLine();
+
+            System.Console.WriteLine("Digite o valor do produto");
+            produtos[i].valor = double.Parse(Console.ReadLine());
+
+            System.Console.WriteLine("O produto esta em estoque? ");
+            produtos[i].disponivelEmEstoque = bool.Parse(Console.ReadLine());
+        }
+        for(int i = 0; i < produtos.Length; i++)
+        {
+            if(produtos[i].disponivelEmEstoque)
+            {
+                System.Console.WriteLine(produtos[i].nome);
+                System.Console.WriteLine(produtos[i].valor);
+            }
+        }
+    }
+}
